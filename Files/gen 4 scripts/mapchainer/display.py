5a29f4c23e36ce164047f8a80c123a910563c321
Jorik Devreese <jorik.devreese@student.howest.be>
2021-06-17 20:54:12 +0200

    Simulation of mapdata finished

--- Files/gen 4 scripts/mapchainer/display.py
+++ Files/gen 4 scripts/mapchainer/display.py
@@ -1,7 +1,9 @@
 import json
+import time
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget,QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget,QPushButton,QMessageBox
+from PyQt5.QtCore import Qt
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QIcon,QFont
 
@@ -15,49 +17,146 @@ class App(QWidget):
     def __init__(self):
         super().__init__()
         self.title = "Map ID router by RETIRE"
-        self.left = 120
-        self.top = 120
-        self.width = 1200
-        self.height = 1000
+        self.left = 20
+        self.top = 40
+        self.width = 1600
+        self.height = 980
 
-        self.sq_sz = 40
+        self.square_sizes = [40,60]
         self.hex = False
-        self.boxes = []
+        self.ram_addresses = []
+        self.directions = []
+        self.update_on_move = True
 
         self.initUI()
 
 
     def initUI(self):
         self.setWindowTitle(self.title)
+        self.setStyleSheet("background-color: #111")
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.draw_boxes()
+        self.prepare_directions()
+        self.prepare_settings()
+        self.prepare_ram()
+        self.update_ram(False)
+        self.setChildrenFocusPolicy(Qt.NoFocus)
         self.show()
-        
 
-    def draw_boxes(self):
+    def setChildrenFocusPolicy (self, policy):
+        def recursiveSetChildFocusPolicy (parentQWidget):
+            for childQWidget in parentQWidget.findChildren(QWidget):
+                childQWidget.setFocusPolicy(policy)
+                recursiveSetChildFocusPolicy(childQWidget)
+        recursiveSetChildFocusPolicy(self)
+
+    def keyPressEvent (self, event):
+        if event.key() == 16777234:
+            self.direction_request("left")
+        if event.key()  == 16777235:
+            self.direction_request("up")
+        if event.key() == 16777236:
+            self.direction_request("right")
+        if event.key()  == 16777237:
+            self.direction_request("down")
+
+        if event.key()  == 32:
+            self.load_selected_map()
+
+    def prepare_ram(self):
+        for i in range(len(mapdata.ram_section)):
+            self.ram_addresses.append(QPushButton(str(i), self))
+            self.ram_addresses[i].resize(self.square_sizes[0],self.square_sizes[0])
+            self.ram_addresses[i].move(10+self.square_sizes[0]*(i%30),10+self.square_sizes[0]*(i//30))
+            self.ram_addresses[i].setFont(QFont("Arial", 10))
+
+    def prepare_directions(self):
+        directions = ["up","left","down","right"]
+        direction_signs = ["^","v",">","<"]
+        for i in range(4):
+            self.directions.append(QPushButton(directions[i],self))
+            self.directions[i].resize(self.square_sizes[1],self.square_sizes[1])
+            self.directions[i].setText(directions[i])
+            self.directions[i].setStyleSheet(f"border-width: 0px; border-style: solid;background-color : white")
+            self.directions[i].setFont(QFont("Arial", 6))
+        
+        self.directions[0].clicked.connect(lambda:self.direction_request(directions[0])) # for some reason this affects all buttons when inside a loop
+        self.directions[1].clicked.connect(lambda:self.direction_request(directions[1]))
+        self.directions[2].clicked.connect(lambda:self.direction_request(directions[2]))
+        self.directions[3].clicked.connect(lambda:self.direction_request(directions[3]))
+
+        self.directions[0].move(1400,830)
+        self.directions[1].move(1330,900)
+        self.directions[2].move(1400,900)
+        self.directions[3].move(1470,900)
+
+
+    def prepare_settings(self):
+        options = ["steps","maps"]
+        
+    def direction_request(self,direction):
+        address = mapdata.pos_to_offset() - 2250
+        self.reset_map_color(address)
+        mapdata.move_player(direction)
+        address = mapdata.pos_to_offset() - 2250
+        self.update_map_color(address)
+        if self.update_on_move:
+            self.load_selected_map()
+
+    def reset_map_color(self,address):
+        if (address >= 0) & (address < len(self.ram_addresses)) :
+            background_color = mapdata.map_id_to_color(mapdata.ram_section[address])
+            self.ram_addresses[address].setStyleSheet(f"border-width: 0px; border-style: solid;background-color : {background_color}")
+            self.ram_addresses[address].setFont(QFont("Arial", 6))
+
+    def load_selected_map(self):
+        address = mapdata.pos_to_offset() - 2250 
+        if (address >= 0) & (address < len(self.ram_addresses)):       
+                    mapdata.map_ids = [mapdata.ram_section[address]]
+                    mapdata.load_consecutive_maps(mapdata.map_ids)
+                    self.update_ram()
+            
+
+
+
+    def update_map_color(self,address,background_color="white"):
+        if (address >= 0) & (address < len(self.ram_addresses)):
+
+            self.ram_addresses[address].setStyleSheet(f"border-width: 0px; border-style: solid;background-color : {background_color}")
+            self.ram_addresses[address].setFont(QFont("Arial", 6))
+
+    def update_ram(self,show=True):
         with open("Files/gen 4 scripts/mapchainer/ramdumps.json","r") as file:
             json_obj = json.load(file)
             for i in range(len(mapdata.ram_section)):
-                confirm_value = json_obj[str(mapdata.test_id)][i]
-                if mapdata.ram_section[i] != confirm_value:
-                    if self.hex == True:
-                        self.boxes.append(QPushButton(f"{hex(mapdata.ram_section[i])},{confirm_value}|", self))
-                    else: 
-                        self.boxes.append(QPushButton(f"{mapdata.ram_section[i]},{confirm_value}|", self))
-                else:
-                    if self.hex == True:
-                        self.boxes.append(QPushButton(str(hex(mapdata.ram_section[i])), self))
-                    else: 
-                        self.boxes.append(QPushButton(str(mapdata.ram_section[i]), self))
-                self.boxes[i].resize(self.sq_sz,self.sq_sz)
-                self.boxes[i].move(self.sq_sz*(i%30)+5,5+self.sq_sz*(i//30))
+
+                # confirm_value = json_obj[str(0)][i]
+                # if mapdata.ram_section[i] != confirm_value:
+                #     if self.hex == True:
+                #         self.ram_addresses.append(QPushButton(f"{hex(mapdata.ram_section[i])},{confirm_value}|", self))
+                #     else: 
+                #         self.ram_addresses.append(QPushButton(f"{mapdata.ram_section[i]},{confirm_value}|", self))
+                # else:
+
+                if self.hex == True:
+                    self.ram_addresses[i].setText(str(hex(mapdata.ram_section[i])))
+                else: 
+                    self.ram_addresses[i].setText(str(mapdata.ram_section[i]))
+
                 # print(self.map_id_to_color(self.ram_section[i]))
-                if mapdata.ram_section[i] != confirm_value:
-                    self.boxes[i].setStyleSheet(f"border-width: 0px; border-style: solid;background-color : white")
-                else:
-                    self.boxes[i].setStyleSheet(f"border-width: 0px; border-style: solid;background-color : {mapdata.map_id_to_color(mapdata.ram_section[i])} ")
-                self.boxes[i].setFont(QFont("Arial", 6))
+                # if mapdata.ram_section[i] != confirm_value:
+                #     self.ram_addresses[i].setStyleSheet(f"border-width: 0px; border-style: solid;background-color : white")
+                # else:
+
+                background_color = mapdata.map_id_to_color(mapdata.ram_section[i])
+                if i == mapdata.pos_to_offset()-2250:
+                    background_color = "white"
+                self.ram_addresses[i].setStyleSheet(f"border-width: 0px; border-style: solid;background-color : {background_color}")
+                self.ram_addresses[i].setFont(QFont("Arial", 6))
         file.close()
+        if show:
+            self.show()
+
+
 
     
 if __name__ == '__main__':
--- Files/gen 4 scripts/mapchainer/mapdataRepository.py
+++ Files/gen 4 scripts/mapchainer/mapdataRepository.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 class mapdataRepository():
     def __init__(self,path_map_data = "Files/gen 4 scripts/mapchainer/map_data.json",path_map_properties="Files/gen 4 scripts/mapchainer/map_colors.json",aslr=0x226D314):
@@ -9,8 +10,15 @@ class mapdataRepository():
         self.address = 0
         self.ram_section = [0]*(24*30) # assuming starting with clean data, and moving into a jubilife map as first id
         self.set_ram_header(self.aslr)
-        self.test_id = 2
-        self.load_ram_data(self.test_id)
+
+        self.x_pos = 7
+        self.y_pos = 2423
+
+        self.steps = 1
+        self.multiply_steps = True
+
+        self.map_ids = [3]
+        self.load_consecutive_maps(self.map_ids)
 
     def map_id_to_color(self,map_id):
         with open(self.path_map_properties,"r") as file:
@@ -56,19 +64,34 @@ class mapdataRepository():
         address = self.address
         instances = ["objects","sprites","warps","triggers"] # all data present in mapdata
         current_instance = 0
+        
         with open(self.path_map_data,"r") as file:
             json_obj = json.load(file)
             if map_id > 558: # error handler converts invalid map id's to 3
                 map_id = 3
+
+
+            for _ in range(4): # clear header data
+                self.set_length_instance_in_header(current_instance,0)
+                self.set_pointer_instance_in_header(current_instance)
+                self.ram_section[address] = 0x0 # add length of instance before instance data
+                address += 1
+                self.ram_section[address] = 0x0
+                address += 1
+                current_instance += 1    
+            
+            current_instance = 0
+            address = self.address
+
             for map_data_type,data_instance in json_obj[str(map_id)].items():
                 if map_data_type in instances:
                     while map_data_type != instances[current_instance]: # if the instance is not in the dump, add 0 as length in header
                         self.set_length_instance_in_header(current_instance,0)
-                        self.set_pointer_instance_in_header(current_instance)
-                        self.ram_section[address] = 0x0 # add length of instance before instance data
-                        address += 1
+                        self.set_pointer_instance_in_header(current_instance)                        
                         self.ram_section[address] = 0x0
-                        address += 1
+                        for _ in range(2):
+                            self.ram_section[address] = 0  # add length of instance before instance data
+                            address += 1
                         current_instance += 1
 
                     self.set_length_instance_in_header(current_instance,len(data_instance))
@@ -80,14 +103,50 @@ class mapdataRepository():
                     self.ram_section[address] = 0x0
                     address += 1
 
-
                     for data in data_instance.values():
                         for value in data.values():
                             self.ram_section[address] = value # add all values of instance
                             address += 1
 
-            file.close()
-            return self.ram_section
+        while current_instance < 4:
+            for _ in range(2):
+                self.ram_section[address] = 0 # add length of instance before (nonexistent) instance
+                address += 1
+            current_instance += 1
+            
+        file.close()
+        return self.ram_section
+
+    def load_consecutive_maps(self,map_id_list,waitperiod=0):
+        for map_id in map_id_list:
+            self.load_ram_data(map_id)
+            time.sleep(waitperiod)
+
+    def pos_to_offset(self,map_width=30):
+        self.x_offs = (self.x_pos // 32)
+        self.y_offs = (self.y_pos //32)*30
+        return self.x_offs + self.y_offs
+    
+    def offset_to_pos(self,map_width=30):
+        self.x_pos = self.x_offs * 32
+        self.y_pos = self.y_offs * 32 // 30
+
+    def move_player(self,direction):
+        directions = ["up","left","down","right"]
+        if self.multiply_steps == True:
+            steps = self.steps * 32
+        if type(direction) == str:
+            if direction.lower() in directions:
+                if direction.lower() == directions[0]:
+                    self.y_pos -= steps
+                if direction.lower() == directions[1]:
+                    self.x_pos -= steps
+                if direction.lower() == directions[2]:
+                    self.y_pos += steps
+                if direction.lower() == directions[3]:
+                    self.x_pos += steps
+                return self.x_pos , self.y_pos
+
 
     @staticmethod
     def split_32_bit(value,endian_type=0):
