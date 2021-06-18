012502a9ad43db857d5091c8a9436ba95a77777a
Jorik Devreese <jorik.devreese@student.howest.be>
2021-06-18 09:45:45 +0200

    more caching

--- Files/gen 4 scripts/mapchainer/mapdataRepository.py
+++ Files/gen 4 scripts/mapchainer/mapdataRepository.py
@@ -64,62 +64,65 @@ class mapdataRepository():
         self.ram_section[self.address_pointer_instances +2*instance_id] = pointer_l  # add pointer of instance in header
         self.ram_section[self.address_pointer_instances +2*instance_id+1] = pointer_h
 
-    def load_ram_data(self,map_id):
-        address = self.address
-        instances = ["objects","sprites","warps","triggers"] # all data present in mapdata
-        current_instance = 0
-        
+    @lru_cache
+    def get_map_data(self,map_id):
         with open(self.path_map_data,"r") as file:
             json_obj = json.load(file)
             if map_id > 558: # error handler converts invalid map id's to 3
                 map_id = 3
 
+            json_data = json_obj[str(map_id)].items()
+        file.close()   
+        return json_data
 
-            for _ in range(4): # clear header data
-                self.set_length_instance_in_header(current_instance,0)
-                self.set_pointer_instance_in_header(current_instance)
-                self.ram_section[address] = 0x0 # add length of instance before instance data
+    def load_ram_data(self,map_id):
+        address = self.address
+        instances = ["objects","sprites","warps","triggers"] # all data present in mapdata
+        current_instance = 0
+        
+        for _ in range(4): # clear header data
+            self.set_length_instance_in_header(current_instance,0)
+            self.set_pointer_instance_in_header(current_instance)
+            self.ram_section[address] = 0x0 # add length of instance before instance data
+            address += 1
+            self.ram_section[address] = 0x0
+            address += 1
+            current_instance += 1    
+        
+        current_instance = 0
+        address = self.address
+
+        json_data = self.get_map_data(map_id)
+        for map_data_type,data_instance in json_data:
+            if map_data_type in instances:
+                while map_data_type != instances[current_instance]: # if the instance is not in the dump, add 0 as length in header
+                    self.set_length_instance_in_header(current_instance,0)
+                    self.set_pointer_instance_in_header(current_instance)                        
+                    self.ram_section[address] = 0x0
+                    for _ in range(2):
+                        self.ram_section[address] = 0  # add length of instance before instance data
+                        address += 1
+                    current_instance += 1
+
+                self.set_length_instance_in_header(current_instance,len(data_instance))
+                self.set_pointer_instance_in_header(current_instance,address)
+                current_instance += 1
+
+                self.ram_section[address] = len(data_instance) # add length of instance before instance data
                 address += 1
                 self.ram_section[address] = 0x0
                 address += 1
-                current_instance += 1    
-            
-            current_instance = 0
-            address = self.address
 
-            for map_data_type,data_instance in json_obj[str(map_id)].items():
-                if map_data_type in instances:
-                    while map_data_type != instances[current_instance]: # if the instance is not in the dump, add 0 as length in header
-                        self.set_length_instance_in_header(current_instance,0)
-                        self.set_pointer_instance_in_header(current_instance)                        
-                        self.ram_section[address] = 0x0
-                        for _ in range(2):
-                            self.ram_section[address] = 0  # add length of instance before instance data
-                            address += 1
-                        current_instance += 1
-
-                    self.set_length_instance_in_header(current_instance,len(data_instance))
-                    self.set_pointer_instance_in_header(current_instance,address)
-                    current_instance += 1
-
-                    self.ram_section[address] = len(data_instance) # add length of instance before instance data
-                    address += 1
-                    self.ram_section[address] = 0x0
-                    address += 1
-
-                    for data in data_instance.values():
-                        for value in data.values():
-                            self.ram_section[address] = value # add all values of instance
-                            address += 1
+                for data in data_instance.values():
+                    for value in data.values():
+                        self.ram_section[address] = value # add all values of instance
+                        address += 1
 
         while current_instance < 4:
             for _ in range(2):
                 self.ram_section[address] = 0 # add length of instance before (nonexistent) instance
                 address += 1
             current_instance += 1
-            
-        file.close()
-        return self.ram_section
 
     def load_consecutive_maps(self,map_id_list,waitperiod=0):
         for map_id in map_id_list:
--- Files/gen 4 scripts/mapchainer/testfile.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-testclass = np.array([0]*10000)
-print(testclass)
